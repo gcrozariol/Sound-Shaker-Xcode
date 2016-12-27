1ea3fee4a960dc6b3180a9ddba9898910efbc061
Guilherme Crozariol <guilherme@guilhermes-macbook-pro.local>
2016-12-27 21:14:33 -0200

    Sound Shaker.

--- Sound Shaker/ViewController.swift
+++ Sound Shaker/ViewController.swift
@@ -7,19 +7,39 @@
 //
 
 import UIKit
+import AVFoundation
+import AudioToolbox
 
 class ViewController: UIViewController {
+    
+    var player = AVAudioPlayer()
 
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        
+        if event?.subtype == UIEventSubtype.motionShake {
+            
+            let fileLocation = Bundle.main.path(forResource: "whip_crack", ofType: "mp3")
+            
+            do {
+                
+                try player = AVAudioPlayer(contentsOf: URL(fileURLWithPath: fileLocation!))
+                player.play()
+                AudioServicesPlayAlertSound(SystemSoundID(kSystemSoundID_Vibrate))
+                    
+                } catch {
+                
+                    // Error Message
+                    
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
-
 }
-
